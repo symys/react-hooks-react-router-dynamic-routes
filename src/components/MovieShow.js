29726c222ed13a8b58562e2c7a94264e51a21edd
SeymaDemir <seymademir35@gmail.com>
2022-12-11 17:39:46 +0300

    done

---
+++ src/components/MovieShow.js
@@ -0,0 +1,18 @@
+// ./src/components/MovieShow.js
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function MovieShow({movies}) {
+    const params = useParams();
+    console.log(params);
+
+  return (
+    <div>
+      {/* And here we access the `movieId` stored in `params` to render 
+        information about the selected movie */}
+      <h3>{movies[params.movieId].title}</h3>
+    </div>
+  );
+}
+
+export default MovieShow;
